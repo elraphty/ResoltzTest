e9a4ab868eeaba7954ed65b69d0da5adb9ddf8f4
elraphty <elraphtyofficial@gmail.com>
2019-05-25 21:47:29 +0000

    finished all unit tests

--- test/reducers/timeReducer.test.js
+++ test/reducers/timeReducer.test.js
@@ -6,23 +6,71 @@ import {
     SET_MINUTES,
     SET_HOURS,
     SET_DAYS
-} from '../../src/redux/reducers/timeReducer';
+} from '../../src/redux/actions/types';
 
 import timeReducer from '../../src/redux/reducers/timeReducer';
 
-console.log(timeReducer);
+let state = {
+    activeTime: 0,
+    activeSession: 'days',
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    days: 0
+}
 
-describe('Request Reducer', () => {
+describe('Time Reducer', () => {
 
-    it('has a default state', () => {
-        expect(timeReducer(undefined, {type: INCREASE_TIME})) === {
-            activeTime: 1000,
+    // get the default state of redux
+    test('has a default state', () => {
+        expect(timeReducer(undefined, { type: undefined })).toEqual({
+            activeTime: 0,
             activeSession: 'days',
             seconds: 0,
             minutes: 0,
             hours: 0,
             days: 0
-        }
+        });
+    });
+
+    // set session to minutes i.e the default session will change from days to minutes
+    test('changes session', () => {
+        expect(timeReducer(undefined, { type: SET_SESSION, payload: 'minutes' }))
+            .toEqual({
+                activeTime: 0,
+                activeSession: 'minutes',
+                seconds: 0,
+                minutes: 0,
+                hours: 0,
+                days: 0
+            });
+    });
+
+    // increase time by 1000 i.e the default session will change from 0 to 1000
+    test('changes active time to 1000', () => {
+        expect(timeReducer(undefined, { type: INCREASE_TIME}))
+            .toEqual({
+                activeTime: 1000,
+                activeSession: 'days',
+                seconds: 0,
+                minutes: 0,
+                hours: 0,
+                days: 0
+            });
+    });
+
+
+     // decrease time by 1000 i.e the default session will change from 1000 to 0
+     test('changes active time to -1000', () => {
+        expect(timeReducer(undefined, { type: DECREASE_TIME}))
+            .toEqual({
+                activeTime: 0,
+                activeSession: 'days',
+                seconds: 0,
+                minutes: 0,
+                hours: 0,
+                days: 0
+            });
     });
 
 });
